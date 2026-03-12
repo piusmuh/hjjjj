b0a579e28db0aeb93f55d6b9420a582f0453494c
piusmuh <muhzgood2@gmail.com>
2026-03-12 12:02:03 +0300

    Add files via upload

---
+++ learn.c
@@ -0,0 +1,29 @@
+#include <stdio.h>  //for input output
+#include <stdbool.h> //for the bool
+
+int main(){
+  int age;
+  
+
+  /*int age = 25;
+  int year = 2025;
+  float gpa = 3.7;
+  char grade = 'A';
+  char name[] = "Pius";
+  double pi = 3.14677899987;
+  bool isOnline = true;
+  prinff("%d\n", isOnline);
+  printf("You are %d years old in %d\n", age, year );
+  printf("Your gpa in %.1f and pi is %.10lf\n", gpa, pi);
+  printtf("Hello %s, your grade in %c\n ", name, grade );*/
+
+  printf("Enter your age: ");
+  scanf("%d", &age);
+  
+
+
+
+
+
+  return 0;
+}
---
+++ learn.py
@@ -0,0 +1,24 @@
+#rows = int(input("Input the number of rows: "))
+#columns = int(input("Input the number of columns: "))
+#symbol = input("Input number symbols: ")
+
+#for y in range(rows):
+  #  for x in range(columns):
+ #       print(symbol, end=" ")
+#   print()
+
+fruits = ['apple', 'mango', 'orange']
+
+#for fruit in fruits:
+ # print(fruit)
+
+#print(fruits[::2])
+#print(dir(fruits))
+#print(help(fruits)
+# 
+#fruits.append('pinapple')
+#fruits.pop()
+#fruits.remove('orange')
+#fruits[0] = 'pineapple'
+fruits.insert(0, 'pineapple')
+print(fruits)
